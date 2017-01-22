41a3bba884b89b05f792bb9df9ab9a22ff9282ab
ieatcrayolaaa <dsgbdomingo@gmail.com>
2017-01-22 19:38:52 +0800

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
-  
+  #resoures is a collection of similar objects (eg.Animals, Humans, Names, etc) in our case articles
   root 'welcome#index'
+  #root '[name of controller#name of file(index.html.erb)]'
 
 end
